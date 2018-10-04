c639c81609f1e6a17e230806ce90866ffe913a0e
AlexanderEklof <37870212+AlexanderEklf@users.noreply.github.com>
2018-10-04 14:00:46 +0200

    vet ej

--- Labb_2/Labb_2/emitter.c
+++ Labb_2/Labb_2/emitter.c
@@ -2,67 +2,40 @@
 
 #include "global.h"
 int i = 0;
-char ch = ' ';
-int index = 0;
-int j = 0;
-int stack[10];
 
 //Riktig jävla spagetthi-band AID lösning.
 //Kolla upp postfix
-void push()
-{
-	stack[j] = arr[index];
-	j++;
-	index++;
-}
-
-int pop(int i)
-{
-
-
-	return i;
-}
-
 int postfixalgorithm(int arr[])
 {
-
+	int stack[10];
 	int sum = 0;
-	index = 0;
-	j = 0;
-	i = 0;
+	int index = 0;
+	int j = 0;
 
 	while (index < 10) {
 		switch (arr[index]) {
 		case '+':
-			//pop
 			stack[j - 2] = stack[j - 2] + stack[j - 1];
 			j--;
 			index++;
 			break;
 		case '-':
-			//pop
 			stack[j - 1] = stack[j - 1] - stack[j];
 			index++;
 			j--;
 			break;
 
 		case '*':
-			//pop
 			stack[j - 2] = stack[j - 2] * stack[j - 1];
 			index++;
 			j--;
 			break;
 
-		case '/':
-			//pop
-			stack[j - 2] = stack[j - 2] / stack[j - 1];
-			index++;
-			j--;
-			break;
 		case 1: case 2: case 3: case 4: case 5: case 6: case'7': case '8': case '9':
 
-			//push
-			push();
+			stack[j] = arr[index];
+			j++;
+			index++;
 			break;
 
 		default:
@@ -79,7 +52,6 @@ int postfixalgorithm(int arr[])
 
 void emit(int t, int tval)  /*  generates output  */
 {
-	
 	switch (t) {
 	case '+': case '-': case '*': case '/':
 		printf("%c\n", t);
@@ -91,7 +63,7 @@ void emit(int t, int tval)  /*  generates output  */
 		printf("%c\n", t);
 		arr[i] = t;
 		i++;
-		printf("%c contains: %d\n",ch, postfixalgorithm(arr));
+		printf("aa: %d\n", postfixalgorithm(arr));
 		break;
 	case DIV:
 		printf("DIV\n");
@@ -104,7 +76,6 @@ void emit(int t, int tval)  /*  generates output  */
 		i++;
 		break;
 	case ID:
-		ch = &(symtable[tval].lexptr);
 		printf("%s\n", symtable[tval].lexptr);
 		break;
 	default:
